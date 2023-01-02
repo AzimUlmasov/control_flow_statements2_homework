3be8f9048e00497ac23cf7fbb721a2c65ffb8900
Azim Ulmasov <azimulmasov2@gmail.com>
2023-01-02 22:32:15 +0500

    if03

--- if03.py
+++ if03.py
@@ -8,12 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    if a>b and a>c or a<b and a<c:
+    if a>b and a<c or a<b and a>c:
         return a
     
-    elif b>c and b>a or b<c and b<a:
+    elif b>a and b<c or b<a and b>c:
         return b
-    
     else:
         return c
 
